d8cff62ecb5e544604efa32e2b52381bc4cbf7ea
Insu Hong <hinsu93@naver.com>
2023-11-04 15:00:28 +0900

    Clsx 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import { cn } from "./libs/utils";
+
 function App() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
-      <div className="h-full bg-slate-400">React Sandbox</div>
+      <div className="h-full bg-slate-400">
+        <button
+          onClick={() => setToggle((prev) => !prev)}
+          className={cn(
+            "bg-sky-300 p-2 rounded-md m-2",
+            toggle && "text-red-500"
+          )}
+        >
+          Clsx Test
+        </button>
+      </div>
     </>
   );
 }
